e8c3639ef1e21682342c5e60378291a2938134ad
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-08-01 17:39:38 +0530

    Js action added for jumbotron

---
+++ Covid19-Tracker/src/main/resources/static/js/actions.js
@@ -0,0 +1,8 @@
+var i = 0;
+function change() {
+  var doc = document.getElementById("background");
+  var color = ["#1e272e","#747d8c","#ff4757","#1B9CFC","#7bed9f","#ff793f","#eb3b5a"];
+  doc.style.backgroundColor = color[i];
+  i = (i + 1) % color.length;
+}
+setInterval(change,5000);
